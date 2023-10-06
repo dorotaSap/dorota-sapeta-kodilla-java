251a056023e40323c3a7ded5c5c4fbac8e15e700
Dorota Sapeta <dodosap@gazeta.pl>
2023-10-06 17:39:16 +0200

    Zadanie 7.4

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WorldTestSuite {
+    @Test
+    void testGetPeopleQuantity(){
+//        Given
+        Country poland = new Country("Poland",new BigDecimal("12000000"));
+        Country usa = new Country("USA",new BigDecimal("130000000"));
+        Country italy = new Country("Italy", new BigDecimal("14000000"));
+        Country china = new Country("China", new BigDecimal("150000000"));
+
+        Continent europe = new Continent("Europe", Arrays.asList(poland, italy));
+        Continent northAmerica = new Continent("North America", Arrays.asList(usa));
+        Continent asia = new Continent("Asia", Arrays.asList(china));
+
+        World world = new World(Arrays.asList(europe,northAmerica,asia));
+
+//        When
+        BigDecimal expectedPopulation = new BigDecimal("306000000");
+        BigDecimal actualPopulation = world.getPeopleQuantity();
+
+//        Then
+        assertEquals(expectedPopulation, actualPopulation);
+
+    }
+}
