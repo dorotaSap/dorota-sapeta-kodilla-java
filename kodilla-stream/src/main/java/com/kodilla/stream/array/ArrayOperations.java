f48f46ab7d0dc31a9e65162c95a9f2790c154bd9
Dorota Sapeta <dodosap@gazeta.pl>
2023-10-08 01:17:53 +0200

    Zadanie 7.5

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+     static double getAverage(int[] numbers){
+         IntStream.range(0, numbers.length)
+                 .mapToObj(i->numbers[i])
+                 .forEach(System.out::println);
+
+         double average = IntStream.range(0, numbers.length)
+                 .mapToDouble(i -> numbers[i])
+                 .average()
+                 .orElse(0.0);
+         return average;
+     }
+
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    void testGetAverage(){
+//        Given
+        int[] numbers = {2,3,5,21,4,56,3,26,37,10,11,22,31,41,15,16,41,42,50,8};
+//        When
+        double average = ArrayOperations.getAverage(numbers);
+//        Then
+        assertEquals(22.2, average, 0.001);
+
+
+    }
+}
