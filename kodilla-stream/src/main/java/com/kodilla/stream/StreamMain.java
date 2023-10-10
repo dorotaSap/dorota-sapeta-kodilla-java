608e03e7c79ed8eda22baa7ca0a290e9fb47ee33
Dorota Sapeta <dodosap@gazeta.pl>
2023-10-10 21:09:18 +0200

    Zadanie 7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,19 +6,21 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
+
     public static void main(String[] args) {
-        Forum forum = new Forum();
+     Forum forum = new Forum();
 
         Map<Integer, ForumUser> filteredUsers = forum.getList().stream()
-                .filter(user -> user.getSex() == 'M')
+               .filter(user -> user.getSex() == 'M')
                 .filter(user -> user.getDateOfBirth().plusYears(20).isBefore(LocalDate.now()))
                 .filter(user -> user.getNumberOfPost() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
-        System.out.println("# elements: " + filteredUsers.size());             // [2]
+       System.out.println("# elements: " + filteredUsers.size());             // [2]
         filteredUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue().getUserName())                   // [3]
-                .forEach(System.out::println);
+               .forEach(System.out::println);
+   }
 
-    }
+
 }
 
